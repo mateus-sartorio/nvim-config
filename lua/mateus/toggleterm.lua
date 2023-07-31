c7d9b62099a69812c7964ba19facbad327ffe402
MateusSartorio <mateussartorio39@gmail.com>
2023-07-31 14:02:03 -0300

    Some tweaks to toggleterm config

--- lua/mateus/toggleterm.lua
+++ lua/mateus/toggleterm.lua
@@ -39,23 +39,26 @@ end
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
-function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
-end
+-- Those lines of code commented below are functions to toggle stuff I don't use/don't have installed
 
-local node = Terminal:new({ cmd = "node", hidden = true })
+--[[ local lazygit = Terminal:new({ cmd = "lazygit", hidden = true }) ]]
 
-function _NODE_TOGGLE()
-	node:toggle()
-end
+--[[ function _LAZYGIT_TOGGLE() ]]
+--[[ 	lazygit:toggle() ]]
+--[[ end ]]
 
-local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
+--[[ local node = Terminal:new({ cmd = "node", hidden = true }) ]]
 
-function _NCDU_TOGGLE()
-	ncdu:toggle()
-end
+--[[ function _NODE_TOGGLE() ]]
+--[[ 	node:toggle() ]]
+--[[ end ]]
+--[[]]
+--[[ local ncdu = Terminal:new({ cmd = "ncdu", hidden = true }) ]]
+--[[]]
+--[[ function _NCDU_TOGGLE() ]]
+--[[ 	ncdu:toggle() ]]
+--[[ end ]]
 
 local htop = Terminal:new({ cmd = "htop", hidden = true })
 
