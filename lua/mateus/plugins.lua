9287e6aafb2b0a777ee771dd59ee620f8184d23f
MateusSartorio <mateussartorio39@gmail.com>
2023-11-12 14:08:26 -0300

    Added start screen

--- lua/mateus/plugins.lua
+++ lua/mateus/plugins.lua
@@ -147,6 +147,25 @@ return packer.startup(function(use)
 		end,
 	})
 
+	use({
+		"nvimdev/dashboard-nvim",
+		event = "VimEnter",
+		config = function()
+			require("dashboard").setup({
+				theme = "hyper", --  theme is doom and hyper default is hyper
+				disable_move = false, --  default is false disable move keymap for hyper
+				shortcut_type = "letter", --  shorcut type 'letter' or 'number'
+				change_to_vcs_root = false, -- default is false,for open file in hyper mru. it will change to the root of vcs
+				config = {}, --  config used for theme
+				hide = {
+					statusline = true, -- hide statusline default is true
+					tabline = true, -- hide the tabline
+					winbar = true, -- hide winbar
+				},
+			})
+		end,
+		requires = { "nvim-tree/nvim-web-devicons" },
+	})
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
