fba64488208e5230d00a4fc9b7e010fade27540e
MateusSartorio <mateussartorio39@gmail.com>
2023-11-11 23:52:16 -0300

    Workaround to nvim-tree not working

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ require("mateus.treesitter")
 require("mateus.autopairs")
 require("mateus.comment")
 require("mateus.gitsigns")
+require("mateus.nvim-tree")
 require("mateus.bufferline")
 require("mateus.toggleterm")
 require("mateus.indent-blankline")
@@ -16,9 +17,73 @@ require("mateus.lint")
 require("mateus.formatter")
 require("mateus.statusbar")
 
-local status_ok, nvim_tree = pcall(require, "mateus.nvim-tree")
-if not status_ok then
-	return
-end
-
-nvim_tree.setup({})
+require("nvim-tree").setup({
+	disable_netrw = true,
+	hijack_netrw = true,
+	open_on_tab = false,
+	auto_close = true,
+	hijack_cursor = false,
+	update_cwd = true,
+	hijack_directories = {
+		enable = true,
+		auto_open = true,
+	},
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
+	view = {
+		width = 30,
+		hide_root_folder = false,
+		side = "left",
+		number = false,
+		relativenumber = false,
+	},
+	renderer = {
+		highlight_git = true,
+		root_folder_modifier = ":t",
+		icons = {
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+			},
+			glyphs = {
+				default = "",
+				symlink = "",
+				git = {
+					unstaged = "",
+					staged = "✓",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "U",
+					ignored = "◌",
+				},
+				folder = {
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+				},
+			},
+		},
+	},
+})
